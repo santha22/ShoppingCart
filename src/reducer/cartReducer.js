85d93a7dabc1b134bb07c0ca84540a1a7ebd4d7a
Savara Santha Kumar <savarasanthakumar20@gmail.com>
2024-02-22 18:51:43 +0530

    null

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -118,7 +118,7 @@ const cartReducer = (state, action) => {
     }
 
     if(action.type === "CART_TOTAL_PRICE") {
-        let total_price = state.cart.reduce((initialVal, curElem) => {
+        let total_price = state.cart?.reduce((initialVal, curElem) => {
             let {price, amount} = curElem;
 
             initialVal = initialVal + price * amount;
