44f4f8cdbcd6a42a42589929b4ab8b2f264c7ffa
Savara Santha Kumar <savarasanthakumar20@gmail.com>
2024-02-22 20:09:42 +0530

    changed

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,3 +1,75 @@
+// import { createContext, useContext, useEffect, useReducer } from "react";
+// import reducer from "../reducer/cartReducer";
+
+// const CartContext = createContext();
+
+// const getLocalCartData = () => {
+//     let localCartData = localStorage.getItem("shoppingCart");
+
+//     if(localCartData?.length === 0) {
+//         return []
+//     } else {
+//         return JSON.parse(localCartData);
+//     }
+// }
+
+// const initialState = {
+//     cart: getLocalCartData(),
+//     total_item: 0,
+//     total_price: 0,
+//     shipping_fee: 50000,
+// }
+
+// const CartProvider = ({children}) => {
+//     const [state, dispatch] = useReducer(reducer, initialState);
+
+//     const removeItem = (id) => {
+//         dispatch({ type: "REMOVE_ITEM", payload: id})
+//     }
+
+//     // clear the cart 
+//     const clearCart = () => {
+//         dispatch({type: "CLEAR_CART"});
+//     }
+
+//     useEffect(() => {
+//         const cartData = getLocalCartData();
+//     })
+
+//     const addToCart = (id, amount, product) => {
+//         dispatch({type: "ADD_TO_CART", payload: {id, amount, product}});
+//     }
+
+//     const setDecrease = (id) => {
+//         dispatch({type: "SET_DECREMENT", payload: id})
+//     }
+
+//     const setIncrease = (id) => {
+//         dispatch({type: "SET_INCREMENT", payload: id})
+//     }
+
+
+
+//     // to add data in localStorage 
+//     useEffect(() => {
+//         dispatch({type: "CART_TOTAL_PRICE"});
+//         localStorage.setItem("shoppingCart", JSON.stringify(state.cart));
+//     }, [state.cart]);
+
+//     return (
+//         <CartContext.Provider value={{...state, addToCart, removeItem, clearCart, setDecrease, setIncrease }}>
+//             {children}
+//         </CartContext.Provider>
+//     )
+// }
+
+
+// const useCartContext = () => {
+//     return useContext(CartContext);
+// }
+
+// export { CartProvider, useCartContext };
+
 import { createContext, useContext, useEffect, useReducer } from "react";
 import reducer from "../reducer/cartReducer";
 
@@ -6,8 +78,8 @@ const CartContext = createContext();
 const getLocalCartData = () => {
     let localCartData = localStorage.getItem("shoppingCart");
 
-    if(localCartData?.length === 0) {
-        return []
+    if (localCartData?.length === 0) {
+        return [];
     } else {
         return JSON.parse(localCartData);
     }
@@ -20,45 +92,42 @@ const initialState = {
     shipping_fee: 50000,
 }
 
-const CartProvider = ({children}) => {
+const CartProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     const removeItem = (id) => {
-        dispatch({ type: "REMOVE_ITEM", payload: id})
+        dispatch({ type: "REMOVE_ITEM", payload: id })
     }
 
     // clear the cart 
     const clearCart = () => {
-        dispatch({type: "CLEAR_CART"});
+        dispatch({ type: "CLEAR_CART" });
     }
 
     const addToCart = (id, amount, product) => {
-        dispatch({type: "ADD_TO_CART", payload: {id, amount, product}});
+        dispatch({ type: "ADD_TO_CART", payload: { id, amount, product } });
     }
 
     const setDecrease = (id) => {
-        dispatch({type: "SET_DECREMENT", payload: id})
+        dispatch({ type: "SET_DECREMENT", payload: id })
     }
 
     const setIncrease = (id) => {
-        dispatch({type: "SET_INCREMENT", payload: id})
+        dispatch({ type: "SET_INCREMENT", payload: id })
     }
-
-
 
     // to add data in localStorage 
     useEffect(() => {
-        dispatch({type: "CART_TOTAL_PRICE"});
+        dispatch({ type: "CART_TOTAL_PRICE" });
         localStorage.setItem("shoppingCart", JSON.stringify(state.cart));
     }, [state.cart]);
 
     return (
-        <CartContext.Provider value={{...state, addToCart, removeItem, clearCart, setDecrease, setIncrease }}>
+        <CartContext.Provider value={{ ...state, addToCart, removeItem, clearCart, setDecrease, setIncrease }}>
             {children}
         </CartContext.Provider>
     )
 }
-
 
 const useCartContext = () => {
     return useContext(CartContext);
